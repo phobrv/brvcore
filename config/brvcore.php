0e264073b0bfa020d55ad470d8c6d2db527589e4
MrPho <nguyentrongpho@gmail.com>
2021-05-31 16:41:16 +0700

    update

--- config/brvcore.php
+++ config/brvcore.php
@@ -4,4 +4,9 @@ return [
 	'robots_file' => '/robots.txt',
 	'htaccess_file' => '/.htaccess',
 	'customize_css_file' => '/css/customize.css',
+	'post_status' => [
+		'publish' => '1',
+		'private' => '0',
+		'draft' => '-1',
+	],
 ];
