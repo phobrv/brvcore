8b7e16df879bdea73e86df066275a4691477d829
Phont <nguyentrongpho@gmail.com>
2021-10-24 16:30:59 +0700

    update

--- src/BrvCoreServiceProvider.php
+++ src/BrvCoreServiceProvider.php
@@ -5,127 +5,136 @@ namespace Phobrv\BrvCore;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
-class BrvCoreServiceProvider extends ServiceProvider {
+class BrvCoreServiceProvider extends ServiceProvider
+{
 
-	public function boot(): void{
+    public function boot(): void
+    {
 
-		Gate::before(function ($user, $ability) {
-			return $user->hasRole('SuperAdmin') ? true : null;
-		});
+        Gate::before(function ($user, $ability) {
+            return $user->hasRole('SuperAdmin') ? true : null;
+        });
 
-		view()->composer(
-			'phobrv::*',
-			'Phobrv\BrvCore\ViewComposers\AdminComposer'
-		);
+        view()->composer(
+            'phobrv::*',
+            'Phobrv\BrvCore\ViewComposers\AdminComposer'
+        );
 
-		$this->loadRepositories();
-		$this->migrations();
-		$this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'phobrv');
-		$this->loadViewsFrom(__DIR__ . '/../resources/views', 'phobrv');
-		$this->loadRoutesFrom(__DIR__ . '/routes.php');
+        $this->loadRepositories();
+        $this->migrations();
+        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'phobrv');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'phobrv');
+        $this->loadRoutesFrom(__DIR__ . '/routes.php');
 
-		if ($this->app->runningInConsole()) {
-			$this->bootForConsole();
-		}
+        if ($this->app->runningInConsole()) {
+            $this->bootForConsole();
+        }
 
-	}
+    }
 
-	public function register(): void{
-		$this->app->singleton(\Phobrv\BrvCore\ViewComposers\AdminComposer::class);
-		$this->mergeConfigFrom(__DIR__ . '/../config/brvcore.php', 'brvcore');
-		$this->mergeConfigFrom(__DIR__ . '/../config/sidebar.php', 'sidebar');
-		$this->mergeConfigFrom(__DIR__ . '/../config/option.php', 'option');
-		$this->mergeConfigFrom(__DIR__ . '/../config/langCode.php', 'langCode');
-		$this->mergeConfigFrom(__DIR__ . '/../config/term.php', 'term');
-		$this->mergeConfigFrom(__DIR__ . '/../config/permission.php', 'permission');
+    public function register(): void
+    {
+        $this->app->singleton(\Phobrv\BrvCore\ViewComposers\AdminComposer::class);
+        $this->mergeConfigFrom(__DIR__ . '/../config/brvcore.php', 'brvcore');
+        $this->mergeConfigFrom(__DIR__ . '/../config/sidebar.php', 'sidebar');
+        $this->mergeConfigFrom(__DIR__ . '/../config/option.php', 'option');
+        $this->mergeConfigFrom(__DIR__ . '/../config/langCode.php', 'langCode');
+        $this->mergeConfigFrom(__DIR__ . '/../config/term.php', 'term');
+        $this->mergeConfigFrom(__DIR__ . '/../config/permission.php', 'permission');
+        $this->mergeConfigFrom(__DIR__ . '/../config/mess.php', 'mess');
 
-		$this->defineMiddleware();
+        $this->defineMiddleware();
 
-		$this->app->singleton('brvcore', function ($app) {
-			return new CoreAdmin;
-		});
-	}
+        $this->app->singleton('brvcore', function ($app) {
+            return new CoreAdmin;
+        });
+    }
 
-	public function provides() {
-		return ['brvcore'];
-	}
+    public function provides()
+    {
+        return ['brvcore'];
+    }
 
-	protected function bootForConsole(): void{
-		// Publishing the configuration file.
-		$this->publishes([
-			__DIR__ . '/../config/option.php' => config_path('option.php'),
-		], 'brvcore.config');
-		$this->publishes([
-			__DIR__ . '/../config/sidebar.php' => config_path('sidebar.php'),
-		], 'brvsidebar.config');
+    protected function bootForConsole(): void
+    {
+        // Publishing the configuration file.
+        $this->publishes([
+            __DIR__ . '/../config/option.php' => config_path('option.php'),
+        ], 'brvcore.config');
+        $this->publishes([
+            __DIR__ . '/../config/sidebar.php' => config_path('sidebar.php'),
+        ], 'brvsidebar.config');
 
-		// Publishing assets.
-		$this->publishes([
-			__DIR__ . '/../resources/assets/' => public_path('vendor/phobrv'),
-		], 'brvcore.assets');
-		// Publishing the translation files.
-		$this->publishes([
-			__DIR__ . '/../resources/lang' => resource_path('lang'),
-		], 'brvcore.lang');
+        // Publishing assets.
+        $this->publishes([
+            __DIR__ . '/../resources/assets/' => public_path('vendor/phobrv'),
+        ], 'brvcore.assets');
+        // Publishing the translation files.
+        $this->publishes([
+            __DIR__ . '/../resources/lang' => resource_path('lang'),
+        ], 'brvcore.lang');
 
-		// Publishing the views.
-		// $this->publishes([
-		//  __DIR__ . '/../resources/views' => base_path('resources/views/vendor/phobrv'),
-		// ], 'brvcore.views');
+        // Publishing the views.
+        // $this->publishes([
+        //  __DIR__ . '/../resources/views' => base_path('resources/views/vendor/phobrv'),
+        // ], 'brvcore.views');
 
-		// Registering package commands.
-		// $this->commands([]);
-	}
+        // Registering package commands.
+        // $this->commands([]);
+    }
 
-	public function migrations() {
-		$this->loadMigrationsFrom(__DIR__ . '/../database/Migrations');
-		// $this->publishes([
-		//  __DIR__ . '/../database/Migrations/' => database_path('migrations'),
-		// ], 'brvcore.migrations');
-	}
+    public function migrations()
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/Migrations');
+        // $this->publishes([
+        //  __DIR__ . '/../database/Migrations/' => database_path('migrations'),
+        // ], 'brvcore.migrations');
+    }
 
-	public function defineMiddleware() {
-		app('router')->aliasMiddleware('lang', \Phobrv\BrvCore\Http\Middleware\Lang::class);
-	}
+    public function defineMiddleware()
+    {
+        app('router')->aliasMiddleware('lang', \Phobrv\BrvCore\Http\Middleware\Lang::class);
+    }
 
-	protected function loadRepositories() {
-		$this->app->bind(
-			\Phobrv\BrvCore\Repositories\UserRepository::class,
-			\Phobrv\BrvCore\Repositories\UserRepositoryEloquent::class
-		);
-		$this->app->bind(
-			\Phobrv\BrvCore\Repositories\TermRepository::class,
-			\Phobrv\BrvCore\Repositories\TermRepositoryEloquent::class
-		);
-		$this->app->bind(
-			\Phobrv\BrvCore\Repositories\PostRepository::class,
-			\Phobrv\BrvCore\Repositories\PostRepositoryEloquent::class
-		);
-		$this->app->bind(
-			\Phobrv\BrvCore\Repositories\OptionRepository::class,
-			\Phobrv\BrvCore\Repositories\OptionRepositoryEloquent::class
-		);
-		$this->app->bind(
-			\Phobrv\BrvCore\Repositories\ReceiveDataRepository::class,
-			\Phobrv\BrvCore\Repositories\ReceiveDataRepositoryEloquent::class
-		);
-		$this->app->bind(
-			\Phobrv\BrvCore\Repositories\CommentRepository::class,
-			\Phobrv\BrvCore\Repositories\CommentRepositoryEloquent::class
-		);
+    protected function loadRepositories()
+    {
+        $this->app->bind(
+            \Phobrv\BrvCore\Repositories\UserRepository::class,
+            \Phobrv\BrvCore\Repositories\UserRepositoryEloquent::class
+        );
+        $this->app->bind(
+            \Phobrv\BrvCore\Repositories\TermRepository::class,
+            \Phobrv\BrvCore\Repositories\TermRepositoryEloquent::class
+        );
+        $this->app->bind(
+            \Phobrv\BrvCore\Repositories\PostRepository::class,
+            \Phobrv\BrvCore\Repositories\PostRepositoryEloquent::class
+        );
+        $this->app->bind(
+            \Phobrv\BrvCore\Repositories\OptionRepository::class,
+            \Phobrv\BrvCore\Repositories\OptionRepositoryEloquent::class
+        );
+        $this->app->bind(
+            \Phobrv\BrvCore\Repositories\ReceiveDataRepository::class,
+            \Phobrv\BrvCore\Repositories\ReceiveDataRepositoryEloquent::class
+        );
+        $this->app->bind(
+            \Phobrv\BrvCore\Repositories\CommentRepository::class,
+            \Phobrv\BrvCore\Repositories\CommentRepositoryEloquent::class
+        );
 
-		$this->app->bind(
-			\Phobrv\BrvCore\Repositories\PostMetaRepository::class,
-			\Phobrv\BrvCore\Repositories\PostMetaRepositoryEloquent::class
-		);
+        $this->app->bind(
+            \Phobrv\BrvCore\Repositories\PostMetaRepository::class,
+            \Phobrv\BrvCore\Repositories\PostMetaRepositoryEloquent::class
+        );
 
-		// $this->app->bind(
-		//  \Phobrv\BrvCore\Repositories\CrawlerDataRepository::class,
-		//  \Phobrv\BrvCore\Repositories\CrawlerDataRepositoryEloquent::class
-		// );
-		// $this->app->bind(
-		//  \Phobrv\BrvCore\Repositories\CrawlerProfileRepository::class,
-		//  \Phobrv\BrvCore\Repositories\CrawlerProfileRepositoryEloquent::class
-		// );
-	}
+        // $this->app->bind(
+        //  \Phobrv\BrvCore\Repositories\CrawlerDataRepository::class,
+        //  \Phobrv\BrvCore\Repositories\CrawlerDataRepositoryEloquent::class
+        // );
+        // $this->app->bind(
+        //  \Phobrv\BrvCore\Repositories\CrawlerProfileRepository::class,
+        //  \Phobrv\BrvCore\Repositories\CrawlerProfileRepositoryEloquent::class
+        // );
+    }
 }
--- src/Services/SendGridService.php
+++ src/Services/SendGridService.php
@@ -4,105 +4,117 @@ namespace Phobrv\BrvCore\Services;
 use SendGrid;
 use SendGrid\Mail\Mail as SendGridEmail;
 
-class SendGridService {
-	private $sendGrid;
-	private $sendGridEmail;
-	private $tos;
-	private $from;
-	private $fromName;
-	private $subject;
-	private $content;
-	private $layout;
-	private $data;
+class SendGridService
+{
+    private $sendGrid;
+    private $sendGridEmail;
+    private $tos;
+    private $from;
+    private $fromName;
+    private $subject;
+    private $content;
+    private $layout;
+    private $data;
 
-	public function __construct() {
-		$this->sendGrid = new SendGrid(env('SENDGRID_API_KEY'));
-		$this->sendGridEmail = new SendGridEmail();
-		$this->tos = '';
-		$this->from = env('MAIL_FROM');
-		$this->fromName = env('MAIL_NAME');
-		$this->content = '';
-		$this->layout = 'emails.layout';
-	}
+    public function __construct()
+    {
+        $this->sendGrid = new SendGrid(config('mess.SENDGRID_API_KEY'));
+        $this->sendGridEmail = new SendGridEmail();
+        $this->tos = '';
+        $this->from = config('mess.MAIL_FROM');
+        $this->fromName = config('mess.MAIL_NAME');
+        $this->content = '';
+        $this->layout = config('mess.MAIL_LAYOUT');
+    }
 
-	public function sendMail($data) {
-		if (isset($data['fromName'])) {
-			$this->setFromName($data['fromName']);
-		}
-		$this->setTos($data['tos']);
-		$this->setData(['title' => $data['title'], 'content' => $data['content']]);
-		$this->setLayout($data['layout'] ?? $this->layout);
-		$this->setSubject($data['subject']);
-		$this->setContent($data['content']);
-		$this->send();
+    public function sendMail($data)
+    {
+        if (isset($data['fromName'])) {
+            $this->setFromName($data['fromName']);
+        }
+        $this->setTos($data['tos']);
+        $this->setData(['title' => $data['title'], 'content' => $data['content']]);
+        $this->setLayout($data['layout'] ?? $this->layout);
+        $this->setSubject($data['subject']);
+        $this->setContent($data['content']);
+        $this->send();
 
-	}
+    }
 
-	public function send() {
+    public function send()
+    {
 
-		$content = $this->getContent();
+        $content = $this->getContent();
 
-		$this->sendGridEmail->setFrom($this->from, $this->fromName);
-		$this->sendGridEmail->setSubject($this->subject);
-		foreach ($this->tos as $_to) {
-			$this->sendGridEmail->addTo($_to);
-		}
+        $this->sendGridEmail->setFrom($this->from, $this->fromName);
+        $this->sendGridEmail->setSubject($this->subject);
+        foreach ($this->tos as $_to) {
+            $this->sendGridEmail->addTo($_to);
+        }
 
-		$this->sendGridEmail->addContent(
-			"text/html", $content
-		);
-		try {
-			$response = $this->sendGrid->send($this->sendGridEmail);
-		} catch (Exception $e) {
-			echo 'Caught exception: ' . $e->getMessage() . "\n";
-		}
-		return false;
-	}
+        $this->sendGridEmail->addContent(
+            "text/html", $content
+        );
+        try {
+            $response = $this->sendGrid->send($this->sendGridEmail);
+        } catch (Exception $e) {
+            echo 'Caught exception: ' . $e->getMessage() . "\n";
+        }
+        return false;
+    }
 
-	public function setTos($tos) {
-		$this->tos = $tos;
+    public function setTos($tos)
+    {
+        $this->tos = $tos;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function getTos() {
-		return $this->tos;
-	}
+    public function getTos()
+    {
+        return $this->tos;
+    }
 
-	public function setSubject($subject) {
-		$this->subject = $subject;
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function setContent($content) {
-		$this->content = $content;
+    public function setContent($content)
+    {
+        $this->content = $content;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function setData($data) {
-		$this->data = $data;
+    public function setData($data)
+    {
+        $this->data = $data;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function setLayout($layoutPath) {
-		$this->layout = $layoutPath;
+    public function setLayout($layoutPath)
+    {
+        $this->layout = $layoutPath;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function setFromName($fromName) {
-		$this->fromName = $fromName;
+    public function setFromName($fromName)
+    {
+        $this->fromName = $fromName;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function getContent() {
-		$content = view($this->layout, $this->data)->render();
+    public function getContent()
+    {
+        $content = view($this->layout, $this->data)->render();
 
-		return $content;
-	}
+        return $content;
+    }
 
 }
