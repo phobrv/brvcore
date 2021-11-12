30bed72de2c3121181d22985b0c137a70ff6988e
Nguyễn Trọng Phổ <phont@arrow-tech.vn>
2021-11-12 12:37:08 +0700

    update

--- resources/views/post/components/statusLabel.blade.php
+++ resources/views/post/components/statusLabel.blade.php
@@ -2,7 +2,7 @@
 	@if($post->status  == 1)
 	<i class="fa fa-check" style="color:green;" title="Active"></i>
 	@elseif ($post->status == 2)
-	<i class="fa fa-star" style="color:yellow;" title="Hot"></i>
+	<i class="fa fa-star" style="color:orange;" title="Hot"></i>
 	@else
 	<i class="fa fa-times-circle" style="color:red" title="Private"></i>
 	@endif
