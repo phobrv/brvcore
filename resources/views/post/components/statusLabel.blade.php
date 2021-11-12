a8507ebd7812e4cb7e22caf9406061a768c9f007
Nguyễn Trọng Phổ <phont@arrow-tech.vn>
2021-11-12 12:34:45 +0700

    update

--- resources/views/post/components/statusLabel.blade.php
+++ resources/views/post/components/statusLabel.blade.php
@@ -1,7 +1,7 @@
 <a id="status{{ $post->id }}" href="javascript:changeStatus('{{ $post->id }}')">
 	@if($post->status  == 1)
 	<i class="fa fa-check" style="color:green;" title="Active"></i>
-	@elseif($post->status == 2)
+	@elseif ($post->status == 2)
 	<i class="fa fa-star" style="color:yellow;" title="Hot"></i>
 	@else
 	<i class="fa fa-times-circle" style="color:red" title="Private"></i>
