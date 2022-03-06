ff92a5baeaa4f3e1507f2aa1c0b4113b1252c408
Nguyen Trong Pho <nguyentrongpho@gmail.com>
2022-03-06 10:32:34 +0700

    update

--- resources/views/menu/config/video.blade.php
+++ resources/views/menu/config/video.blade.php
@@ -6,8 +6,8 @@
 		<input type="hidden" name="menu_id" value="{{ $data['post']->id }}">
 		@csrf
 		<div class="box-body">
-			@include('phobrv::input.inputSelect',['label'=>'Product Group','key'=>'video_term_paginate','type'=>'meta','array'=>$arrayProductGroup])
-			@include('phobrv::input.inputText',['label'=>'Số lượng sản phẩm hiển thị','key'=>'video_number','inputType'=>'number','type'=>'meta'])
+			@include('phobrv::input.inputSelect',['label'=>'Video Group','key'=>'video_term_paginate','type'=>'meta','array'=>$arrayProductGroup])
+			@include('phobrv::input.inputText',['label'=>'Số lượng video hiển thị','key'=>'video_number','inputType'=>'number','type'=>'meta'])
 		</div>
 		<div class="box-footer">
 			{{ Form::submit('Lưu cấu hình',array('class'=>'btn btn-primary pull-right')) }}
