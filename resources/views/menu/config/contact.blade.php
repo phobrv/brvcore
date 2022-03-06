344dd6e26b07aa228b285ef62e7ef1587641d605
Nguyen Trong Pho <nguyentrongpho@gmail.com>
2022-03-07 00:39:33 +0700

    update

--- resources/views/menu/config/contact.blade.php
+++ resources/views/menu/config/contact.blade.php
@@ -7,7 +7,7 @@
 		@csrf
 		<div class="box-body">
 			@include('phobrv::input.inputSelect',['label'=>'Layout','key'=>'layout','type'=>'meta','array'=>['layout1'=>'Layout 1','layout2'=>'Layout 2']])
-			@include('phobrv::input.inputTextarea',['label'=>'Code GoogleMap','key'=>'code_googlemap','type'=>'meta'])
+			@include('phobrv::input.inputTextarea',['label'=>'Code GoogleMap','key'=>'code_googlemap','type'=>'meta','row'=>6])
 		</div>
 		<div class="box-footer">
 			{{ Form::submit('Lưu cấu hình',array('class'=>'btn btn-primary')) }}
