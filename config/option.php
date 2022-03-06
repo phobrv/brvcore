a5175df57a00774406765d95adb2852597280c05
Nguyen Trong Pho <nguyentrongpho@gmail.com>
2022-03-06 10:30:15 +0700

    update

--- config/option.php
+++ config/option.php
@@ -1,30 +1,31 @@
 <?php
 return [
-	'langMain' => 'vi',
-	'paginate' => '5',
-	'http_type' => env('HTTP_TYPE', 'https'),
-	'templateMenu' => [
-		'0' => '-',
-		'home' => 'Home',
-		'link' => 'Link',
-		'category' => 'Nhóm bài viết',
-		'article' => 'Bài viết',
-		'order' => 'Đặt hàng',
-		'contact' => 'Liên hệ',
-		'product' => 'Nhóm sản phẩm',
-	],
-	'post_type' => [
-		'post' => 'post',
-		'image' => 'image',
-		'menu_item' => 'menu_item',
-		'video' => 'video',
-		'question' => 'question',
-		'customeridea' => 'customeridea',
-		'drugstore' => 'drugstore',
-		'product' => 'product',
-		'recruitment'=>'recruitment',
-		'buildproject'=>'buildproject',
-	],
+    'langMain' => 'vi',
+    'paginate' => '5',
+    'http_type' => env('HTTP_TYPE', 'https'),
+    'templateMenu' => [
+        '0' => '-',
+        'home' => 'Home',
+        'link' => 'Link',
+        'category' => 'Nhóm bài viết',
+        'article' => 'Bài viết',
+        'order' => 'Đặt hàng',
+        'contact' => 'Liên hệ',
+        'product' => 'Nhóm sản phẩm',
+        'video' => 'Nhóm video',
+    ],
+    'post_type' => [
+        'post' => 'post',
+        'image' => 'image',
+        'menu_item' => 'menu_item',
+        'video' => 'video',
+        'question' => 'question',
+        'customeridea' => 'customeridea',
+        'drugstore' => 'drugstore',
+        'product' => 'product',
+        'recruitment' => 'recruitment',
+        'buildproject' => 'buildproject',
+    ],
 ]
 
-;?>
+;
