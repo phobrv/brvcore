f2f6aa1e8a065ff2cced004d4e0a3e3cf6c55b48
Phont <nguyentrongpho@gmail.com>
2022-03-28 23:22:59 +0700

    update

--- config/brvreceive.php
+++ config/brvreceive.php
@@ -1,23 +1,26 @@
 <?php
 
 return [
-	'type' => [
-		'0' => '-',
-		'contact' => 'Liên hệ',
-	],
-	'status' => [
-		'pending' => '0',
-		'fail' => '-1',
-		'success' => '1',
-	],
-	'statusLabel' => [
-		'0' => 'Pending',
-		'-1' => 'Fail',
-		'1' => 'success',
-	],
-	'statusColor' => [
-		'0' => 'orange',
-		'1' => 'green',
-		'-1' => 'red',
-	],
+    'type' => [
+        '0' => '-',
+        'contact' => 'Liên hệ',
+    ],
+    'status' => [
+        'pending' => '0',
+        'fail' => '-1',
+        'success' => '1',
+        'paid' => '2',
+    ],
+    'statusLabel' => [
+        '0' => 'Pending',
+        '-1' => 'Fail',
+        '1' => 'success',
+        '2' => 'Paid',
+    ],
+    'statusColor' => [
+        '0' => 'orange',
+        '1' => 'green',
+        '2' => 'green',
+        '-1' => 'red',
+    ],
 ];
